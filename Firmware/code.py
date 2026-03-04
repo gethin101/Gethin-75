7cece01904273e9c8b358f0b69886eb8c7a6fa1d
gethin101 <gethingrice@gmail.com>
2026-03-04 07:34:21 +0000

    Update code.py

--- Firmware/code.py
+++ Firmware/code.py
@@ -1,4 +1,9 @@
-# KMK - gethin101
+# KMK -  gethin101
+#from kmk.bootcfg import bootcfg
+
+#bootcfg(
+    #usb_id={'manufacturer': "Gethin101", 'product': "Gethin-75"},
+#)
 
 import board
 from kmk.kmk_keyboard import KMKKeyboard
@@ -58,37 +63,37 @@ keyboard.keymap = [
         KC.ESC, KC.F1, KC.F2, KC.F3, KC.F4,
         KC.F5, KC.F6, KC.F7, KC.F8,
         KC.F9, KC.F10, KC.F11, KC.F12,
-        MAC1, MAC2, MAC3, KC.NO, KC.NO,
+        MAC1, MAC2, MAC3, KC.NO,
     ],
 
     [
         KC.GRAVE, KC.N1, KC.N2, KC.N3, KC.N4, KC.N5, KC.N6, KC.N7, KC.N8,
-        KC.N9, KC.N0, KC.MINUS, KC.EQUAL, KC.BSPC, KC.INS, KC.HOME, KC.PGUP, KC.NO,
+        KC.N9, KC.N0, KC.MINUS, KC.EQUAL, KC.BSPC, KC.INS, KC.HOME, KC.PGUP,
     ],
 
     [
         KC.TAB, KC.Q, KC.W, KC.E, KC.R, KC.T, KC.Y, KC.U, KC.I,
         KC.O, KC.P, KC.LBRC, KC.RBRC,
-        KC.ENTER, KC.DEL, KC.END, KC.PGDN, KC.NO,
+        KC.ENTER, KC.DEL, KC.END, KC.PGDN,
     ],
 
     [
         KC.CAPS, KC.A, KC.S, KC.D, KC.F, KC.G, KC.H, KC.J, KC.K,
         KC.L, KC.SCLN, KC.QUOT, KC.NUHS,
-        MAC5, MAC6, MAC7, KC.NO, KC.NO,
+        MAC5, MAC6, MAC7, KC.NO,
     ],
 
     [
         KC.LSFT, KC.NUBS, KC.Z, KC.X, KC.C, KC.V, KC.B, KC.N, KC.M,
         KC.COMMA, KC.DOT, KC.SLSH, KC.RSFT,
-        MAC8, KC.UP, MAC9, KC.NO, KC.NO,
+        MAC8, KC.UP, MAC9, KC.NO,
     ],
 
     [
         KC.LCTL, KC.LGUI, KC.LALT, KC.SPC,
         KC.RALT, KC.RGUI, KC.APP, KC.RCTL,
         KC.LEFT, KC.DOWN, KC.RIGHT,
-        KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,
+        KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,
     ],
 ]
 
